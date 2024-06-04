b6b990004fdfe3e0f33abcde3bf777fa8e57ae83
john <honghaitzz11@gmail.com>
2024-06-04 17:46:00 +0800

    全面改为yarn来开发

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,6 @@
 import {defineConfig} from "vite";
-import path from 'node:path'
 import react from "@vitejs/plugin-react";
 import alias from '@rollup/plugin-alias';
-import vitePluginForArco from '@arco-plugins/vite-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
